187da94b25aec17ed1e7208bf85b3898ee12452f
maximus3 <max2000.gym5cheb@yandex.ru>
2021-03-11 21:31:18 +0300

    hw1/v1

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -4,19 +4,145 @@ namespace Afina {
 namespace Backend {
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Put(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Put(const std::string &key, const std::string &value) {
+    auto element = _lru_index.find(key);
+    if (element == _lru_index.end()) {
+        return putElement(key, value);
+    }
+    SimpleLRU::lru_node& node = element->second.get();
+    return updateNode(node, value);
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) {
+    auto element = _lru_index.find(key);
+    if (element == _lru_index.end()) {
+        return putElement(key, value);
+    }
+    return false;
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Set(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Set(const std::string &key, const std::string &value) {
+    auto element = _lru_index.find(key);
+    if (element == _lru_index.end()) {
+        return false;
+    }
+    SimpleLRU::lru_node& node = element->second.get();
+    return updateNode(node, value);
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Delete(const std::string &key) { return false; }
+bool SimpleLRU::Delete(const std::string &key) {
+    auto element = _lru_index.find(key);
+    if (element == _lru_index.end()) {
+        return false;
+    }
+    SimpleLRU::lru_node& node = element->second.get();
+
+    deleteNode(node);
+
+    return true;
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Get(const std::string &key, std::string &value) { return false; }
+bool SimpleLRU::Get(const std::string &key, std::string &value) {
+    auto element = _lru_index.find(key);
+    if (element == _lru_index.end()) {
+        return false;
+    }
+
+    SimpleLRU::lru_node& node = element->second.get();
+    value = node.value;
+
+    moveNode(node);
+
+    return true;
+}
+
+void SimpleLRU::moveNode(SimpleLRU::lru_node& node) {
+    if (node.next == nullptr) {
+        return;
+    }
+    auto ptr = node.next->prev;
+    ptr->next->prev = ptr->prev; // next ok
+
+    if (ptr->prev == nullptr) {
+        _lru_tail->next = std::move(_lru_head); // tail ok
+        _lru_head = std::move(ptr->next); // head ok
+    } else {
+        _lru_tail->next = std::move(ptr->prev->next); // tail ok
+        ptr->prev->next = std::move(ptr->next); // prev ok
+    }
+
+    ptr->next = nullptr;
+    ptr->prev = _lru_tail;
+    _lru_tail = ptr;
+}
+
+bool SimpleLRU::putElement(const std::string& key, const std::string& value) {
+    std::size_t node_size = key.size() + value.size();
+    if (node_size > _max_size) {
+        return false;
+    }
+    while (_cur_size + node_size > _max_size) {
+        deleteNode(std::ref(*_lru_head));
+    }
+
+    auto node = new lru_node {key, value, _lru_tail, nullptr};
+
+    if (_lru_head == nullptr) {
+        _lru_head = std::unique_ptr<lru_node>(node);
+    } else {
+        _lru_tail->next = std::unique_ptr<lru_node>(node);
+    }
+
+    _lru_tail = node;
+    _cur_size += node_size;
+    _lru_index.emplace(std::cref(_lru_tail->key), std::ref(*node));
+
+    return true;
+}
+
+bool SimpleLRU::updateNode(SimpleLRU::lru_node& node, const std::string& value) {
+    std::size_t new_value_size = value.size();
+    std::size_t old_value_size = node.value.size();
+
+    if (node.key.size() + new_value_size > _max_size) {
+        return false;
+    }
+
+    moveNode(node);
+
+    while (_cur_size - old_value_size + new_value_size > _max_size) {
+        deleteNode(std::ref(*_lru_head));
+    }
+
+    node.value = value;
+    _cur_size += new_value_size - old_value_size;
+
+    return true;
+}
+
+void SimpleLRU::deleteNode(SimpleLRU::lru_node& node) {
+    _cur_size -= (node.key.size() + node.value.size());
+    _lru_index.erase(node.key);
+
+    if (_lru_head.get() == _lru_tail) { // 1 element
+        _lru_head = nullptr;
+        _lru_tail = nullptr;
+    } else if (node.prev == nullptr) { // head
+        _lru_head = std::move(_lru_head->next);
+        _lru_head->prev = nullptr;
+    } else if (node.next == nullptr) {
+        _lru_tail = _lru_tail->prev;
+        _lru_tail->next = nullptr;
+    } else {
+        lru_node* prev_node = node.prev;
+        prev_node->next = std::move(node.next);
+        prev_node->next->prev = prev_node;
+    }
+}
 
 } // namespace Backend
 } // namespace Afina
--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -15,13 +15,23 @@ namespace Backend {
  * # Map based implementation
  * That is NOT thread safe implementaiton!!
  */
+
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    SimpleLRU(size_t max_size = 1024)
+        : _max_size(max_size)
+        , _cur_size(0)
+        , _lru_head(nullptr)
+        , _lru_tail(nullptr) {}
 
     ~SimpleLRU() {
         _lru_index.clear();
-        _lru_head.reset(); // TODO: Here is stack overflow
+
+        while (_lru_head != nullptr) {
+            auto _lru_next = std::move(_lru_head->next);
+            _lru_head.reset(nullptr);
+            _lru_head = std::move(_lru_next);
+        }
     }
 
     // Implements Afina::Storage interface
@@ -42,24 +52,32 @@ public:
 private:
     // LRU cache node
     using lru_node = struct lru_node {
-        std::string key;
+        const std::string key;
         std::string value;
-        std::unique_ptr<lru_node> prev;
+        lru_node* prev;
         std::unique_ptr<lru_node> next;
     };
 
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be not greater than the _max_size
     std::size_t _max_size;
+    std::size_t _cur_size;
 
     // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
     // element that wasn't used for longest time.
     //
     // List owns all nodes
     std::unique_ptr<lru_node> _lru_head;
+    lru_node* _lru_tail;
 
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
+    std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>, std::less<const std::string>> _lru_index;
+
+private:
+    void moveNode(SimpleLRU::lru_node& node);
+    bool putElement(const std::string& key, const std::string& value);
+    bool updateNode(SimpleLRU::lru_node& node, const std::string& value);
+    void deleteNode(SimpleLRU::lru_node& node);
 };
 
 } // namespace Backend
